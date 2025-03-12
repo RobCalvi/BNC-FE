0b51840b0f97ccacaf607e9e2f637d2cb9b1f2bf
Rob Calvi <roberto.jean.calvi@gmail.com>
2025-03-12 08:24:17 -0600

    change apiClien base url

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
-import { BASE_URL } from "../environment";
+// import { BASE_URL } from "../environment";
 
 const apiClient = axios.create({
-    baseURL: BASE_URL
+    baseURL: "https://bnc-be.onrender.com"
 })
 
 
